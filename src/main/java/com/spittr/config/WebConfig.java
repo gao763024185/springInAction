cf2155e86e0e32451712be08d0b00c6aad0cfbcb
gaobh <763024185@qq.com>
2017-08-09 19:30:26 +0800

    2017/08/09 spring实战 第六章 创建JSP视图

--- src/main/java/com/spittr/config/WebConfig.java
+++ src/main/java/com/spittr/config/WebConfig.java
@@ -16,12 +16,15 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @EnableWebMvc
 @ComponentScan("com.spittr.web")
 public class WebConfig extends WebMvcConfigurerAdapter {
+    //基于java配置jsp视图解析器
     @Bean
     public ViewResolver viewResolver(){
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
         resolver.setExposeContextBeansAsAttributes(true);
+        //使用JSTL标签来处理格式化和信息
+        resolver.setViewClass(org.springframework.web.servlet.view.JstlView.class);
         return resolver;
     }
     @Override
